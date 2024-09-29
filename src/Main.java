31e1d8d31a52ffc5f624befc5b90003da8b6afce
naomi <sarkernr@mail.uc.edu>
2024-09-29 00:50:45 -0400

    Ass01 RPS

--- src/Main.java
+++ src/Main.java
@@ -7,28 +7,30 @@ public class Main
 
         String play1 = "";
         String play2 = "";
+        String trash = "";
 
         System.out.println("Player 1, enter your move [RPS]: ");
         play1 = in.nextLine();
         System.out.println("Player 2, enter your move [RPS]: ");
         play2 = in.nextLine();
 
-        if (play1.equalsIgnoreCase("R"))
-            if (play2.equalsIgnoreCase("R"))
-            {
-                System.out.println("Rock vs. Rock, it's a TIE!");
-            }
-            else if (play2.equalsIgnoreCase("P"))
-            {
-                System.out.println("Paper covers Rock, player 2 WINS!");
-            }
-            else if (play2.equalsIgnoreCase("S"))
-            {
-                System.out.println("Rock beats Scissors, player 1 WINS!");
-            }
-
-        else if (play1.equalsIgnoreCase("P"))
+        {
+            if (play1.equalsIgnoreCase("R"))
                 if (play2.equalsIgnoreCase("R"))
+                {
+                    System.out.println("Rock vs. Rock, it's a TIE!");
+                }
+                else if (play2.equalsIgnoreCase("P"))
+                {
+                    System.out.println("Paper covers Rock, player 2 WINS!");
+                }
+                else if (play2.equalsIgnoreCase("S"))
+                {
+                    System.out.println("Rock beats Scissors, player 1 WINS!");
+                }
+
+            if (play1.equalsIgnoreCase("P"))
+                 if (play2.equalsIgnoreCase("R"))
                 {
                     System.out.println("Paper covers Rock, Player 1 WINS!");
                 }
@@ -41,8 +43,8 @@ public class Main
                     System.out.println("Scissor cuts Paper, Player 2 WINS!");
                 }
 
-        else if (play1.equalsIgnoreCase("S"))
-                if (play2.equalsIgnoreCase("R"))
+            if (play1.equalsIgnoreCase("S"))
+                 if (play2.equalsIgnoreCase("R"))
                 {
                     System.out.println("Rock beats Scissors, player 2 WINS!");
                 }
@@ -54,5 +56,25 @@ public class Main
                 {
                     System.out.println("Scissor vs. Scissors, it's a TIE!");
                 }
+
+             else if (play1 == trash)
+            {
+                trash = in.nextLine();
+                System.out.println(play1 + " is not a valid move!");
+                System.out.println("Run the program again with correct input!");
+            }
+            {
+                System.out.println("Do you want to play again? (Y/N)");
+            }
+
+                if (in.nextLine().equalsIgnoreCase("N"))
+                    System.out.println("Have great day!");
+
+                if (in.nextLine().equalsIgnoreCase("Y"))
+                    System.out.println("Rerun the program");
+
+
+        }
+
     }
 }
